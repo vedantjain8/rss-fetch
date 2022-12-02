0851bc8895b6d5f4ca5270d01c1802bfa7e2cd5e
vedant <vedantjain88666@gmail.com>
2022-12-02 13:19:20 +0530

    changed config from rss_site.py to Userdata.yaml & added global logging

--- main.py
+++ main.py
@@ -1,68 +1,67 @@
-import rss_site,rss_fetch,sqlite3,os,datetime,time,re,logging,tool,argparse
+import rss_fetch,sqlite3,os,datetime,time,re,argparse,mylog
+from myyaml import yamldef
 
-def dumpp():
-    def cleanString(s):
+def cleanString(s):
         r = re.sub(r'[^\w]', ' ', s)
         s = re.sub(' +', ' ', r)
         return str(s)
 
+def dumpp():
     start = time.time()
-    
+
     try:
         os.mkdir("logs")
     except:
         pass
 
-    logging.basicConfig(filename=r"logs/logs.log",
-                        format='%(asctime)s %(message)s',
-                        filemode='a')
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-
     db = sqlite3.connect("rss.db")
     cur = db.cursor()
-    tool.dropAllSQLtableTrigger()
     
     # Initialize parser
     parser = argparse.ArgumentParser()
     
     # Adding optional argument
-    parser.add_argument("-k", "--Key", help = "Key from rss_site", default=None)
+    parser.add_argument("-k", "--Key", help = "Key from Userdata.yaml", default=None)
         
     # Read arguments from command line
     args = parser.parse_args()
     
     if args.Key:
-        site_name = str(rss_site.siteDict.get(str(args.Key))[1].title())  
-        site_url = str(rss_site.siteDict.get(str(args.Key))[0])
-        cur.execute(f"create table if not exists {site_name} (id text primary key, title text not null, link text not null,summary text, time text not null, pub_day text not null, pub_date text not null, pub_month text not null, pub_year text not null, pub_time text not null);")
-        cur.execute(f"create trigger if not exists oldPost_delete_{site_name} before insert on {site_name} when (select count(id) from {site_name}) > {int(rss_site.min_articles_to_retain_per_website)} begin delete from {site_name} where id in (select id from {site_name} order by time asc limit 1); end;")
-        posts_details, post_count = rss_fetch.get_posts_details(rss=site_url)
-        for post in posts_details:
-            try:
-                postTitle = cleanString(str(post["title"]))
-                insert_qry = f'insert into {site_name} values ("{post["id"]}", "{postTitle}","{post["link"]}","{post["summary"]}",datetime("now"),"{post["pub_day"]}" ,"{post["pub_date"]}" ,"{post["pub_month"]}","{post["pub_year"]}","{post["pub_time"]}");'
-                cur.execute(insert_qry)
-                #notification
-                db.commit()
-                logger.info(f">> ADDED: {site_name} [+] {postTitle}")
-                
-            except Exception as error:
-                if str(error).split(" ")[0:3] == ['UNIQUE', 'constraint', 'failed:']:
-                    pass
-                else:
-                    logger.error(f">> ERROR: {site_name} [!] {postTitle} -> {error}")
+        try: 
+            site_name =  str(args.Key)
+            site_url = str(yamldef()['sites'][str(args.Key)])
+            cur.execute(f"drop trigger oldPost_delete_{site_name};")
+            cur.execute(f"create table if not exists {site_name} (id text primary key, title text not null, link text not null,summary text, time text not null, pub_day text not null, pub_date text not null, pub_month text not null, pub_year text not null, pub_time text not null);")
+            cur.execute(f"create trigger if not exists oldPost_delete_{site_name} before insert on {site_name} when (select count(id) from {site_name}) > {int(yamldef()['min_articles_to_retain_per_website'])} begin delete from {site_name} where id in (select id from {site_name} order by time asc limit 1); end;")
+            posts_details, post_count = rss_fetch.get_posts_details(rss=site_url)
+            for post in posts_details:
+                try:
+                    postTitle = cleanString(str(post["title"]))
+                    insert_qry = f'insert into {site_name} values ("{post["id"]}", "{postTitle}","{post["link"]}","{post["summary"]}",datetime("now"),"{post["pub_day"]}" ,"{post["pub_date"]}" ,"{post["pub_month"]}","{post["pub_year"]}","{post["pub_time"]}");'
+                    cur.execute(insert_qry)
+                    #notification
+                    db.commit()
+                    mylog.logDef(loggLevel='info', logMSG=f">> ADDED: {site_name} [+] {postTitle}")
+                    
+                except Exception as error:
+                    if str(error).split(" ")[0:3] == ['UNIQUE', 'constraint', 'failed:']:
+                        pass
+                    else:
+                        mylog.logDef(loggLevel='error', logMSG=f">> ERROR: {site_name} [!] {postTitle} -> {error}")
 
-        end = time.time()
-        logger.info(f"Last ran on {datetime.datetime.now()} for {site_name}")
-        logger.info(f"Completed in: {round((end-start)*10**3/1000, 5)} Seconds")
+            end = time.time()
+            mylog.logDef(loggLevel='info', logMSG=f"Last ran on {datetime.datetime.now()} for site: {site_name}")
+            mylog.logDef(loggLevel='info', logMSG=f"Completed in: {round((end-start)*10**3/1000, 5)} Seconds")
+
+        except Exception as argsError:
+            mylog.logDef(loggLevel='error', logMSG=f">> ERROR: {argsError}, maybe there is no site in the given list")
     else:
-        for i in rss_site.siteDict:
-            site_name = str(rss_site.siteDict.get(i)[1].title())  
-            site_url = str(rss_site.siteDict.get(i)[0])
+        for siteI in yamldef()['sites']:
+            site_name = str(siteI)
+            site_url = str(yamldef()['sites'][siteI])
 
             cur.execute(f"create table if not exists {site_name} (id text primary key, title text not null, link text not null,summary text, time text not null, pub_day text not null, pub_date text not null, pub_month text not null, pub_year text not null, pub_time text not null);")
-            cur.execute(f"create trigger if not exists oldPost_delete_{site_name} before insert on {site_name} when (select count(id) from {site_name}) > {int(rss_site.min_articles_to_retain_per_website)} begin delete from {site_name} where id in (select id from {site_name} order by time asc limit 1); end;")
+            cur.execute(f"create trigger if not exists oldPost_delete_{site_name} before insert on {site_name} when (select count(id) from {site_name}) > {int(yamldef()['min_articles_to_retain_per_website'])} begin delete from {site_name} where id in (select id from {site_name} order by time asc limit 1); end;")
             posts_details, post_count = rss_fetch.get_posts_details(rss=site_url)
             for post in posts_details:
                 
@@ -72,17 +71,17 @@ def dumpp():
                     cur.execute(insert_qry)
                     #notification
                     db.commit()
-                    logger.info(f">> ADDED: {site_name} [+] {postTitle}")
+                    mylog.logDef(loggLevel='info', logMSG=f">> ADDED: {site_name} [+] {postTitle}")
                 
                 except Exception as error:
                     if str(error).split(" ")[0:3] == ['UNIQUE', 'constraint', 'failed:']:
                         pass
                     else:
-                        logger.error(f">> ERROR: {site_name} [!] {postTitle} -> {error}")
+                        mylog.logDef(loggLevel='error', logMSG=f">> ERROR: {site_name} [!] {postTitle} -> {error}")
         
         end = time.time()
-        logger.info(f"Last ran on {datetime.datetime.now()}")
-        logger.info(f"Completed in: {round((end-start)*10**3/1000, 5)} Seconds")
+        mylog.logDef(loggLevel='info', logMSG=f"Last ran on {datetime.datetime.now()}")
+        mylog.logDef(loggLevel='info', logMSG=f"Completed in: {round((end-start)*10**3/1000, 5)} Seconds")
 
     db.close()
     
