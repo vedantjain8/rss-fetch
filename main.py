257d7ba82eb8ecafdfc692e2324ba99698e7cf48
vedant <vedantjain88666@gmail.com>
2022-12-06 13:11:32 +0530

    added image column

--- main.py
+++ main.py
@@ -30,14 +30,17 @@ def dumpp():
         try: 
             site_name =  str(args.Key)
             site_url = str(yamldef()['sites'][str(args.Key)])
-            cur.execute(f"drop trigger oldPost_delete_{site_name};")
-            cur.execute(f"create table if not exists {site_name} (id text primary key, title text not null, link text not null,summary text, time text not null, pub_day text not null, pub_date text not null, pub_month text not null, pub_year text not null, pub_time text not null);")
-            cur.execute(f"create trigger if not exists oldPost_delete_{site_name} before insert on {site_name} when (select count(id) from {site_name}) > {int(yamldef()['min_articles_to_retain_per_website'])} begin delete from {site_name} where id in (select id from {site_name} order by time asc limit 1); end;")
+            try:
+                cur.execute(f"drop trigger oldPost_delete_{site_name};")
+            except:
+                pass
+            cur.execute(f"create table if not exists rss_data (id text primary key, title text not null, link text not null,summary text,author text, site text not null, time text not null, pub_day text not null, pub_date text not null, pub_month text not null, pub_year text not null, pub_time text not null);")
+            cur.execute(f"create trigger if not exists oldPost_delete_{site_name} before insert on rss_data when (select count(id) from rss_data where site='{site_name}') > {int(yamldef()['min_articles_to_retain_per_website'])} begin delete from rss_data where id in (select id from rss_data where site='{site_name}' order by time asc limit 1); end;")
             posts_details, post_count = rss_fetch.get_posts_details(rss=site_url)
             for post in posts_details:
                 try:
                     postTitle = cleanString(str(post["title"]))
-                    insert_qry = f'insert into {site_name} values ("{post["id"]}", "{postTitle}","{post["link"]}","{post["summary"]}",datetime("now"),"{post["pub_day"]}" ,"{post["pub_date"]}" ,"{post["pub_month"]}","{post["pub_year"]}","{post["pub_time"]}");'
+                    insert_qry = f'insert into rss_data (id,title,link,summary,time,pub_day,pub_date,pub_month,pub_year,pub_time,author,site) values ("{post["id"]}", "{postTitle}","{post["link"]}","{post["summary"]}",datetime("now"),"{post["pub_day"]}" ,"{post["pub_date"]}" ,"{post["pub_month"]}","{post["pub_year"]}","{post["pub_time"]}","{post["author"]}","{site_name}");'
                     cur.execute(insert_qry)
                     #notification
                     db.commit()
@@ -60,14 +63,13 @@ def dumpp():
             site_name = str(siteI)
             site_url = str(yamldef()['sites'][siteI])
 
-            cur.execute(f"create table if not exists {site_name} (id text primary key, title text not null, link text not null,summary text, time text not null, pub_day text not null, pub_date text not null, pub_month text not null, pub_year text not null, pub_time text not null);")
-            cur.execute(f"create trigger if not exists oldPost_delete_{site_name} before insert on {site_name} when (select count(id) from {site_name}) > {int(yamldef()['min_articles_to_retain_per_website'])} begin delete from {site_name} where id in (select id from {site_name} order by time asc limit 1); end;")
+            cur.execute(f"create table if not exists rss_data (id text primary key, title text not null, link text not null,summary text,author text, site text not null,image text, time text not null, pub_day text not null, pub_date text not null, pub_month text not null, pub_year text not null, pub_time text not null);")
+            cur.execute(f"create trigger if not exists oldPost_delete_{site_name} before insert on rss_data when (select count(id) from rss_data where site='{site_name}') > {int(yamldef()['min_articles_to_retain_per_website'])} begin delete from rss_data where id in (select id from rss_data where site='{site_name}' order by time asc limit 1); end;")
             posts_details, post_count = rss_fetch.get_posts_details(rss=site_url)
-            for post in posts_details:
-                
+            for post in posts_details:  
                 try:
                     postTitle = cleanString(str(post["title"]))
-                    insert_qry = f'insert into {site_name} values ("{post["id"]}", "{postTitle}","{post["link"]}","{post["summary"]}",datetime("now"),"{post["pub_day"]}" ,"{post["pub_date"]}" ,"{post["pub_month"]}","{post["pub_year"]}","{post["pub_time"]}");'
+                    insert_qry = f'insert into rss_data (id,title,link,summary,time,pub_day,pub_date,pub_month,pub_year,pub_time,author,site,image) values ("{post["id"]}", "{postTitle}","{post["link"]}","{post["summary"]}",datetime("now"),"{post["pub_day"]}" ,"{post["pub_date"]}" ,"{post["pub_month"]}","{post["pub_year"]}","{post["pub_time"]}","{post["author"]}","{site_name}","{post["image"]}");'
                     cur.execute(insert_qry)
                     #notification
                     db.commit()
